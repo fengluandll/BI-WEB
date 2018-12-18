2a1c5784d65c33d5c37c8512fd10fb4874816f6b
pipiw <1900836569@qq.com>
2018-12-18 10:29:31 +0800

    xiugai

--- src/routes/EditCharts/ReportBoard.js
+++ src/routes/EditCharts/ReportBoard.js
@@ -528,10 +528,25 @@ class ReportBoard extends PureComponent {
     }
   }
   onMouseEnterShow = (e) => {
-    e.target.style.opacity = 1;
+    if(e.target.style.height === '100%'){
+      e.target.style.opacity = 1;
+      e.target.children[0].style.opacity = 1;
+    }else{
+      e.target.parentNode.style.opacity = 1;
+      e.target.style.opacity = 1;
+    }
   }
   onMouseLeaveHide = (e) => {
-    e.target.style.opacity = 0;
+    if(this.state.editModel == "true"){
+      return;
+    } 
+    if(e.target.style.height === '100%'){
+      e.target.style.opacity = 0;
+      e.target.children[0].style.opacity = 0;
+    }else{
+      e.target.parentNode.style.opacity = 0;
+      e.target.style.opacity = 0;
+    }
 
   }
   /****************************************点击事件*****************************************************************/
@@ -1007,7 +1022,10 @@ class ReportBoard extends PureComponent {
 
     return (
       <div>
-        <div style={{ width: 30, height: 60, border: '2px solid #ccc', borderRadius: 6, borderLeft: '1px solid #ccc', opacity: 0, position: 'fixed', top: '50%', marginTop: -30, left: 0, zIndex: 1000, fontSize: 26, textAlign: 'center', lineHeight: 2, cursor: 'pointer' }} onClick={this.changeEditeMode} onMouseEnter={this.onMouseEnterShow.bind(this)} onMouseLeave={this.onMouseLeaveHide.bind(this)}>||</div>
+        {/* 添加返回按钮的父级 */}
+        <div style={{marginLeft: (this.state.editModel == "true") ? "170px" : "0", width: 30, height: '100%', opacity: 0, position: 'fixed', top: 0, left: 0, zIndex: 1000, fontSize: 26, textAlign: 'center',cursor: 'pointer'}} onClick={this.changeEditeMode} onMouseEnter={this.onMouseEnterShow.bind(this)} onMouseLeave={this.onMouseLeaveHide.bind(this)}>
+          <div style={{marginLeft: (this.state.editModel == "true") ? "170px" : "0", width: 30, height: 60, opacity: '1 !important', border: '2px solid #ccc', borderRadius: 6, borderLeft: '1px solid #ccc', background:'#eee', color:'#000', position: 'fixed', top: '50%', marginTop: -30, left: 0, zIndex: 1000, fontSize: 26, textAlign: 'center', lineHeight: 2, cursor: 'pointer',}} >||</div>
+        </div>
         {this.state.editModel == "true" ? <div className={styles['boardLeft']} ref={(instance) => { this.left = instance; }} > </div> : <div></div>}
         <div id="contents" className={`boardcenter_report`} ref={(instance) => { this.center = instance; }} style={{ paddingLeft: (this.state.editModel == "true") ? "200px" : "0", paddingRight: (this.state.editModel == "true") ? "200px" : "0", background: '#eee' }}>
           {this.renderTab()}
