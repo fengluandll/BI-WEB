7418e2f72f733f3ac14318cab73e1697f04067fa
PetterTom <1206420658@qq.com>
2019-03-13 14:15:29 +0800

    a

---
+++ src/componentsPro/RelationUtil/TabsUI/index.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import ReactDom from 'react-dom';
+import { Form, Input, Button, Modal, Select, Switch, Checkbox, Row, Col } from 'antd';
+
+/***
+ * tabs组件
+ * 页签组件
+ * 
+ * ***/
+class TabsUI extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate() {
+    }
+
+    componentWillReceiveProps(nextProps) {
+    }
+
+    /*************************************************************/
+
+
+    /*******************************************************************/
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TabsUI;
