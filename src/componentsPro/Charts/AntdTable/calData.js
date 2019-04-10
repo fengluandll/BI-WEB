95913fb4a492d0f927bd52cdfaf946c33f933606
PetterTom <1206420658@qq.com>
2019-04-10 17:35:23 +0800

    a

--- src/componentsPro/Charts/AntdTable/calData.js
+++ src/componentsPro/Charts/AntdTable/calData.js
@@ -26,10 +26,11 @@ export default class CalData {
         const head_ret = []; // 返回值head
         const body_ret = []; // 返回值body
         // 列数据
+        const noWidthKey = this.findNoWidth(head, mChart); //不用设置宽度的head的key
         for (let key in head) {
             const rsColumnConf = head[key];
-            const { id, rsc_display } = rsColumnConf;
-            const obj = { "title": rsc_display, "dataIndex": rsc_display, "key": rsc_display, "align": "center" };
+            const { id } = rsColumnConf;
+            const obj = { "rsColumnConf": rsColumnConf, "align": "center" };
             // f1 判断是否url跳转列
             const columnUrl_arr = columnUrl.split(",");
             if (columnUrl_arr.indexOf(id) > 0) { // 该列是跳转url列
@@ -39,26 +40,32 @@ export default class CalData {
             // f2 固定列
             const fixed_left_arr = fixed_left.split(",");
             const fixed_right_arr = fixed_right.split(",");
-            if (fixed_left_arr.indexOf(id) > 0) { // 该列是固定到左侧的列
+            if (fixed_left_arr.indexOf(id) >= 0) { // 该列是固定到左侧的列
                 obj.fixed = "left";
-                obj.width = 300;
-            } else if (fixed_right_arr.indexOf(id) > 0) { // 该列是固定到右侧的列
+                obj.width = 200;
+            } else if (fixed_right_arr.indexOf(id) >= 0) { // 该列是固定到右侧的列
                 obj.fixed = "right";
-                obj.width = 300;
+                obj.width = 200;
             } else {
                 obj.fixed = "none";
             }
             // f3 设置每列的宽度
-            if (forceFit != "1" && key != head.length - 1) { // 如果不是自适应的情况下要显固定头部那么除了最后一列不舍宽度，其他都要设置宽度
+            if (fixed_left_arr.indexOf(id) < 0 && fixed_right_arr.indexOf(id) < 0 && forceFit != "1" && key != noWidthKey) { // 如果不是自适应的情况下要显固定头部那么除了最后一列不舍宽度，其他都要设置宽度
                 obj.width = 200;  // 后期改成根据每列的最大字符数来控制
             }
             // f4 被关联字段要加特殊样式 和 点击事件
             const { type, name, chartId, styleConfig, relation } = item;
-            const relation_keys = Object.keys(relation);
-            if (relation_keys.indexOf(id) > 0) { // 该列字段是关联了其他的图表
-                const style = { backgroundColor: '#F4F4F4' };
-                obj.style = style; // 样式
-                obj.plotParam = id; // 点击参数是当前id
+            if (null != id) { // 假数据的时候rsColumnConf里面没有值,自己后来造的字段也没有rsColumnConf
+                if (null == relation[id]) {
+                    head_ret.push(obj); // 返回之前json数据还是要放的
+                    continue;
+                }
+                const { relationFields } = relation[id]; // 取出当前字段所在的relationFields
+                if (null != relationFields && Object.keys(relationFields).length > 0) { // 该列字段是关联了其他的图表(relationFields里面有对象)
+                    const style = { backgroundColor: '#F4F4F4' };
+                    obj.style = style; // 样式
+                    obj.plotParam = id; // 点击参数是当前id
+                }
             }
             head_ret.push(obj); // 最后放入列数据
         }
@@ -67,7 +74,8 @@ export default class CalData {
             const line = body[key]; // 每行数据
             const line_obj = []; // 返回的每行的数组
             for (let k in line) { // body 第二层循环,循环列
-                const value = line[k]; // 每个数据的值
+                let value = line[k] == null ? "" : line[k]; // 每个数据的值,处理空字符串
+                value = value.toString().replace('\n', ' '); // 替换换行符
                 const obj = { value: value }; // 每个数据 拼成对象
                 // f5 超过12个字符的后面用...表示,并用提示框提示全部内容
                 if (value.length > 12 && config.forceFit != "1") { // 如果字符大于12个的时候那就隐藏用Tooltip提示
@@ -100,6 +108,7 @@ export default class CalData {
             width: '50px',
             dataIndex: '序号',
             key: '序号',
+            fixed: 'left',
             render: (text, record, index) => {
                 return (
                     <div style={{ textAlign: 'center', backgroundColor: '#f3f3f3', marginLeft: '-5px', marginRight: '-5px' }}>{`${index + 1}`}</div>
@@ -109,8 +118,9 @@ export default class CalData {
         columns.push(first_col); // 放入第一行的序号列
         for (let key in head) {
             const obj_head = head[key];
-            const { url, fixed, width, plotParam, style } = obj_head;
-            const obj = { "title": obj_head.title, "dataIndex": obj_head.dataIndex, "key": obj_head.key, "align": obj_head.align };
+            const { rsColumnConf, url, fixed, width, plotParam, style } = obj_head;
+            const rsc_display = rsColumnConf == null ? key : rsColumnConf.rsc_display; // 防止rsColumnConf为空
+            const obj = { "title": rsc_display, "dataIndex": rsc_display, "key": rsc_display, "align": obj_head.align };
             // f1 判断跳转
             if (null != url) {
                 const { param, url } = url;
@@ -133,7 +143,7 @@ export default class CalData {
             if (null != plotParam) {
                 obj.render = (text, record, index) => {
                     return (
-                        <div style={style} onClick={onPlotClickAntTable.bind(this, key, text, mChart.id)}>{text}</div>
+                        <div style={style} onClick={onPlotClickAntTable.bind(this, plotParam, text.value, mChart.id)}>{text}</div>
                     );
                 }
             }
@@ -145,7 +155,7 @@ export default class CalData {
             const obj = { "key": key }; // antd需要的每行json
             const body_line = body[key]; // 每行的中间数据
             for (let k in body_line) {
-                const { title } = head[k]; // 每列数据取出列标题中文
+                const { rsColumnConf } = head[k]; // 每列数据取出列标题中文
                 const line_obj = body_line[k]; // 每个中间数据
                 let { value, style, tooltip } = line_obj;
                 if (null != style) { // 加样式--可能是值预警的样式
@@ -162,12 +172,32 @@ export default class CalData {
                         </Tooltip>
                     );
                 }
-                obj[title] = value; // 最终没什么特殊情况就是普通的显示值就行了
+                obj[rsColumnConf.rsc_display] = value; // 最终没什么特殊情况就是普通的显示值就行了
             }
             data.push(obj);
         }
         tableDate["columns"] = columns;
         tableDate["data"] = data;
         return tableDate;
+    }
+
+    /***
+     * 找到一个不用设置宽度的列
+     * 
+     * ***/
+    findNoWidth = (head, mChart) => {
+        let ret = 100; //返回值
+        const config = JSON.parse(mChart.config);
+        const { columnUrl, columnUrlStr, columnUrlParam, fixed_left, fixed_right, column, forceFit } = config;
+        const fixed_left_arr = fixed_left.split(",");
+        const fixed_right_arr = fixed_right.split(",");
+        for (let key in head) {
+            const rsColumnConf = head[key];
+            const { id } = rsColumnConf;
+            if (fixed_left_arr.indexOf(id) < 0 && fixed_right_arr.indexOf(id) < 0) { // 不是固定列
+                ret = key; // 不是固定列的最后一个字段
+            }
+        }
+        return ret;
     }
 }
